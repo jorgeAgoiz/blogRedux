d13777165c5321e3419ab0f99b1626a82c77e898
jorgeAP <jorgeagoiz@gmail.com>
2021-04-22 18:21:26 +0200

    Little comments added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
-import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
-export const fetchPosts = () => async dispatch => {
-    const response = await jsonPlaceHolder.get('/posts');
+// This function return an async function with dispatch as argument, because
+// redux-thunk will work in this return statement.
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceHolder.get("/posts");
 
-    dispatch({ type: 'FETCH_POSTS', payload: response });
-}
+  dispatch({ type: "FETCH_POSTS", payload: response });
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk"; //Middleware to return async functions
 
-import App from './components/App';
-import reducers from './reducers';
+import App from "./components/App";
+import reducers from "./reducers";
 
+// This is our store, with our reducers and middlewares
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.querySelector('#root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
 );
+
+/* Rules about Reducers:
+    1- Must return any value besides 'undefined'.
+    2- Produces 'state' or data to be used inside of your app
+    using only previous state and the action.
+    3- Must not return reach 'out of itseld' to decide what value to return 
+    (reducers are pure).
+    4- Must not mutate its input 'state' argument, just because if you return the 
+    same old state your application will not re-rendered.
+*/
